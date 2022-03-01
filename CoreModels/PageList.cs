8c35ffad5ee0f3c08cf66ce3bbbaea3806461032
KirillKalutsky <kefirchik.2000@mail.ru>
2022-03-01 11:01:55 +0500

    добавил модель для пагинации

---
+++ CoreModels/PageList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreModels
+{
+    public class PageList<T> : List<T>
+    {
+        public int CurrentPage { get; }
+        public int TotalPages { get; }
+        public int PageSize { get; }
+        public long TotalCount { get; }
+
+        public bool HasPrevious => CurrentPage > 1;
+        public bool HasNext => CurrentPage < TotalPages;
+
+        public PageList(List<T> items, long totalCount, int currentPage, int pageSize)
+        {
+            TotalCount = totalCount;
+            PageSize = pageSize;
+            CurrentPage = currentPage;
+            TotalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+            AddRange(items);
+        }
+    }
+}
