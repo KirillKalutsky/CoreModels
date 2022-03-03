51d1ee78d8c1572e89f8dacdee460fbd0123c6f0
KirillKalutsky <kefirchik.2000@mail.ru>
2022-03-03 11:03:52 +0500

    убрал наследование от листа

--- CoreModels/PageList.cs
+++ CoreModels/PageList.cs
@@ -6,23 +6,24 @@ using System.Threading.Tasks;
 
 namespace CoreModels
 {
-    public class PageList<T> : List<T>
+    public class PageList<T>
     {
         public int CurrentPage { get; }
         public int TotalPages { get; }
         public int PageSize { get; }
         public long TotalCount { get; }
+        public IEnumerable<T> Items { get; }
 
         public bool HasPrevious => CurrentPage > 1;
         public bool HasNext => CurrentPage < TotalPages;
 
-        public PageList(List<T> items, long totalCount, int currentPage, int pageSize)
+        public PageList(IEnumerable<T> items, long totalCount, int currentPage, int pageSize)
         {
             TotalCount = totalCount;
             PageSize = pageSize;
             CurrentPage = currentPage;
             TotalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
-            AddRange(items);
+            Items = items;
         }
     }
 }
