17f0a3bd82d522060d1fc1aed84363e46ed7d28d
KirillKalutsky <kefirchik.2000@mail.ru>
2022-05-03 16:16:20 +0500

    changes

---
+++ CoreModels/Crawl/PagesArchitectureSiteMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreModels.Crawl
+{
+    public class PagesArchitectureSiteMethods
+    {
+        public MethodGetPageUrl getPageUrl;
+    }
+
+    public enum MethodGetPageUrl
+    {
+
+    }
+
+
+}
