f394bd48bb037d787e5ceb48166622edd810ee83
KirillKalutsky <kefirchik.2000@mail.ru>
2022-05-04 13:05:48 +0500

    date

--- CoreModels/Crawl/UrlCreator/DatePageUrlCreator.cs
+++ CoreModels/Crawl/UrlCreator/DatePageUrlCreator.cs
@@ -13,7 +13,7 @@ namespace CoreModels.Crawl.UrlCreator
         }
         public override string CreatePageUrl(int pageNumber)
         {
-            var date = DateTime.Now.AddDays(-1*pageNumber);
+            var date = DateTime.Now.AddDays(-1*(pageNumber-1));
             string dateStr;
             if (datePattern == null)
                 dateStr = ((DateTimeOffset)date).ToUnixTimeSeconds().ToString();
